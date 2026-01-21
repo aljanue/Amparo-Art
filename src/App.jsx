d6bddd4d890ca84217349dcf5e55a71190bb6b30
Alberto Játiva <134947748+aljanue@users.noreply.github.com>
2026-01-21 17:26:42 +0100

    added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Gallery from "./pages/Gallery";
@@ -10,6 +11,7 @@ import Order from "./pages/Order";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="grow">
